1e32ae1635bbfcf55ac9dcc79e63478379af6cd1
Coldbirdie <l.t.stein@st.hanze.nl>
2024-05-23 09:06:44 +0200

    Documenting docstrings; params and returns

--- Assignment1/assignment1.py
+++ Assignment1/assignment1.py
@@ -9,6 +9,7 @@ import multiprocessing as mp
 import os
 from collections import defaultdict
 
+
 def process_prompt_params():
     """
     Using argparse enables this script to receive parameters
@@ -57,6 +58,10 @@ def process_prompt_params():
 def read_binary_chunk(filename, start, end):
     """
     Reads binary file and slice of a chunk (by substraction)
+    param: filename, str
+    param: start, int; start of chunk
+    param: end, int, end of chunk
+    :return: a binary chunk of the file
     """
     with open(filename, mode="rb") as binary_file:
         # "The Python File seek() method sets the file's cursor at a specified position in the current file"
@@ -89,7 +94,7 @@ def convert_binary_to_phredscores(chunk):
     numeric_values = defaultdict(list)  # every new key gets a list!
     for index, line in enumerate(lines, start=0):
         if index % 4 == 3:  # select fourth line additivel
-            #print(line.strip())
+            # print(line.strip())
             numeric_phredscores: list = process_to_numeric(line.strip())
             for base_position, score in enumerate(numeric_phredscores, start=0):
                 numeric_values[base_position].append(
@@ -108,12 +113,14 @@ def calculate_average_phredscores(all_phredscores: list):
     average_phredscores_per_base_position = defaultdict(
         list
     )  # new key receives a list!
-    
-    for chunks_results in range(len(all_phredscores)):
+
+    for chunks_results in range(len(all_phredscores)): # pylint: disable=C0200
         for (
             base_position,
             phredscores,
-        ) in all_phredscores[chunks_results].items():  # type: int type: list
+        ) in all_phredscores[
+            chunks_results
+        ].items():  # type: int type: list
             average_phredscores_per_base_position[base_position].extend(
                 phredscores
             )  # merge lists of same base position to new dict
@@ -124,7 +131,9 @@ def calculate_average_phredscores(all_phredscores: list):
             "line_number": base_position,
             "average_phredscore": sum(phredscores) / len(phredscores),
         }
-        for base_position, phredscores in sorted(average_phredscores_per_base_position.items())
+        for base_position, phredscores in sorted(
+            average_phredscores_per_base_position.items()
+        )
     ]
     return average_phredscores
 
@@ -133,6 +142,9 @@ def process_chunk(file_chunk_info):
     """
     Call functions to read a chunk of file in binary mode and
     convert the bytes to numerical phredscores
+    return positional_average_scores
+    param: file_chunk_info, list of file path, start and end of chunk positions
+    :return: numerical phredscores
     """
     file_path, start, end = file_chunk_info
     chunk = read_binary_chunk(file_path, start, end)
@@ -140,27 +152,40 @@ def process_chunk(file_chunk_info):
 
 
 def check_read_completeness(file_path, start, end):
-    """Adjust the start and end positions to align with read boundaries."""
+    """
+    Adjust the start and end positions to align with read boundaries.
+    param: file_path, str
+    param: start, str; starting position of chunk
+    param: end, str: end position of chunk
+    """
     with open(file_path, "rb") as binary_file:
-        if start != 0: # If start is not byte 0, jump to the start of the new chunk
-            binary_file.seek(start) # set the file's cursor at start position
+        if start != 0:  # If start is not byte 0, jump to the start of the new chunk
+            binary_file.seek(start)  # set the file's cursor at start position
             # Move to the start of the next read
             while True:
                 line = binary_file.readline()
-                if not line or line.startswith(b"@"): # stop if end is reached or stop before a new read (preventing overlap between chunks)
-                    break # breaks the true loop
+                if not line or line.startswith(
+                    b"@"
+                ):  # stop if end is reached or stop before a new read (preventing overlap between chunks)
+                    break  # breaks the true loop
             # Jump to the beginning of the first read, to ensure the starting boundary
-            start = binary_file.tell() - len(line) # after reading a line tell() returns the current position of the file pointer
+            start = binary_file.tell() - len(
+                line
+            )  # after reading a line tell() returns the current position of the file pointer
 
         # Adjust end position to the end of the current chunk and only start if not at end of original file
         if end != os.path.getsize(file_path):
-            binary_file.seek(end) # set the file's cursor at start position
+            binary_file.seek(end)  # set the file's cursor at start position
             # Move to the end of the current read
             while True:
                 line = binary_file.readline()
-                if not line or line.startswith(b"@"): # stop if end is reached or stop before a new read (preventing overlap between chunks)
-                    end = binary_file.tell() - len(line) # Jump to beginning of last line
-                    break # breaks the true loop
+                if not line or line.startswith(
+                    b"@"
+                ):  # stop if end is reached or stop before a new read (preventing overlap between chunks)
+                    end = binary_file.tell() - len(
+                        line
+                    )  # Jump to beginning of last line
+                    break  # breaks the true loop
 
     return file_path, start, end
 
@@ -169,11 +194,16 @@ def process_fastq_file(file_path, cpus):
     """
     Split file into chunks and process each chunk in parallel over n cpus.
     It attempts to create 10 chunks and adds the remaining chunk at the end of chunk list.
+    param: file_path, string
+    param: cpus, int
+    :return: average phredscores
     """
     file_size = os.path.getsize(file_path)
     n_chunks = 10
     chunk_size = file_size // n_chunks  # tries 10 chunks
-    uneven_chunk = file_size % n_chunks  # determines remaining number of chunks possible
+    uneven_chunk = (
+        file_size % n_chunks
+    )  # determines remaining number of chunks possible
 
     # tuples of (filename, start e.g. = chunk_size * 0 = 0, end = 0 + 1 * chunk_size = chunk_size or file_size).
     # file_size is only chosen as last in the iteration
@@ -190,41 +220,44 @@ def process_fastq_file(file_path, cpus):
         )
         for i in range(n_chunks)
     ]
-    
+
     with mp.Pool(cpus) as pool:
         results = pool.map(process_chunk, chunks)  # chunks is an iterable
         # , process_chunk_wrapper is invoked n = length(chunk) times
 
         # for each chunk a dicionary with positions as keys and scores from all reads in that chunk as values
         # e.g. 0 : [19, 19, 25, 24] the length of four is four reads on base position
-    positional_average_scores = calculate_average_phredscores(results)
-    return positional_average_scores
+    return calculate_average_phredscores(results)
 
 
 def choose_output_format(phredscores, output_file):
-    """                                                                 
-    Function to decide what to do with presenting the results           
-    to the user. Also, prints after deciding.                           
-    param: output_file : string   
-    """   
+    """
+    Function to decide what to do with presenting the results
+    to the user. Also, prints after deciding.
+    param: output_file : string
+    """
     fieldnames = ["line_number", "average_phredscore"]
-    if output_file: 
+    if output_file:
         with open(output_file, mode="w", encoding="UTF-8") as csvfile:
-            writer = csv.DictWriter(csvfile, fieldnames=fieldnames) # use csv object for writing to a csv file
+            writer = csv.DictWriter(
+                csvfile, fieldnames=fieldnames
+            )  # use csv object for writing to a csv file
             for filename, base_scores in phredscores.items():
                 csvfile.write(filename)
-                for base_average_score in range(len(base_scores)): # pylint : disable=C0200
+                for base_average_score in range(len(base_scores)): # pylint: disable=C0200
                     writer.writerow(base_scores[base_average_score])
-                csvfile.write('\n')
+                csvfile.write("\n")
         print(f"--Successfully written phredscores to {output_file}--")
-    else: # print instead of write
+    else:  # print instead of write
         for filename, base_scores in phredscores.items():
             print(filename + "\n")
-            for base_average_score in range(len(base_scores)):
-                print(f"{base_scores[base_average_score]['line_number']},{base_scores[base_average_score]['average_phredscore']}")
-            print('\n')
+            for base_average_score in range(len(base_scores)): # pylint: disable=C0200
+                print(
+                    f"{base_scores[base_average_score]['line_number']},{base_scores[base_average_score]['average_phredscore']}"
+                )
+            print("\n")
 
-            
+
 if __name__ == "__main__":
     args = process_prompt_params()
 
