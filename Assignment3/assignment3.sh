72ed4609384caf0e32ef57340c66b9d6473ac489
L Stein <60214213+LukaStein@users.noreply.github.com>
2024-06-12 15:44:54 +0200

    Added final step for passing the phredscores to the mean function call
    To prevent memory overload, a maximum block size of 5M is made for a chunk and a job may use a maximum of 1G of its memory. TODO: add the functionality to process multiple files

--- Assignment3/assignment3.sh
+++ Assignment3/assignment3.sh
@@ -1,7 +1,10 @@
-#!/bin/bash:
+#!/bin/bash
+
+# Locate the location of this bash script, and assumely the python script
+LOCATE_DIR=$(dirname -- "$( readlink -f -- "$0"; )")
 
 FILE_PATH=$1
 CORES=$2
 
 
-awk 'NR % 4 == 0' $FILE_PATH | parallel --sshloginfile parallel_ssh -j $CORES --pipe python3 Kwartaal_12/BDC/Assignment3/assignment3.py
+awk 'NR % 4 == 0' $FILE_PATH | parallel --pipe --sshloginfile parallel_ssh --block 5M --memfree 1G -j $CORES python3 ${LOCATE_DIR}/assignment3.py -calc {} | python3 ${LOCATE_DIR}/assignment3.py -mean {}
