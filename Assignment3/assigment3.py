f78c6f6efa12b81dc480f2c37b753bcd0b2f1ac0
L Stein <60214213+LukaStein@users.noreply.github.com>
2024-06-12 15:42:00 +0200

    Python script now works for scores and means
    To deal with the final step, numpy arrays are made for faster processing of the lump of phred score lines. Since without those arrays, thus purely on lists the program is too slow and dies out.

--- Assignment3/assigment3.py
+++ Assignment3/assigment3.py
@@ -1,68 +1,90 @@
 import sys
 import csv
 from collections import defaultdict
+import numpy as np
+import argparse as ap
+
+def process_prompt_params():
+    """
+    Using argparse enables this script to receive parameters
+    such as number of CPUs, fastqfile.fastq (or more in a list)
+    and optionally the name of the output file; otherwise presented
+    with STDOUT to terminal.
+
+    param: -n <number_cpus>
+    param: -o <output csv file>
+    param: fastqfile1.fastq (no keyword required i.e. positional argument)
+    -> can also be a list with several files
+    [fastqfile1.fastq fastqfile2,fastq]
+
+    :return: args object containing all params as objects
+    """
+    argparser = ap.ArgumentParser(
+        description="Script for assignment 1 \
+                                  of Big Data Computing"
+    )
+    argparser.add_argument(
+        "-o",
+        action="store",
+        dest="csvfile",
+        required=False,
+        help="CSV file to save the output to. \
+                          Defaulted output to terminal STDOUT",
+    )
+    argparser.add_argument(
+        "-calc",
+        action='store_true',
+        required=False,
+        help="Option for making phredscore calculations \
+                          Defaulted output to terminal STDOUT",
+    )
+    argparser.add_argument(
+        "-mean",
+        action='store_true',
+        required=False,
+        help="Option to calculate means across base positions \
+                          Defaulted output to terminal STDOUT",
+    )
+    argparser.add_argument(
+        "fastq_files",
+        action="store",
+        nargs="+",
+        help="At least 1 processable \
+                           Illumina Fastq Format file",
+    )
+    return argparser.parse_args()
 
 def process_to_numeric(line: bytes):
     return [byte - 33 for byte in line]
 
-def convert_binary_to_phredscores(chunk):
-    lines = chunk.split(b"\n")
-    lines = [line for line in lines if line]
-    all_basepositions_with_scores = []
-    headers = []
-    for index, line in enumerate(lines, start=0):
-        numeric_phredscores = process_to_numeric(line.strip())
-        headers.append(len(line))
-        scores_per_line = {}
-        for base_position, score in enumerate(numeric_phredscores, start=0):
-            scores_per_line |= {base_position:score}
-        all_basepositions_with_scores.append(scores_per_line)
-    return all_basepositions_with_scores, headers
+def convert_binary_to_phredscores():
+    for line in sys.stdin.buffer:
+        if line.strip():
+            numeric_phredscores = process_to_numeric(line.strip())
+            print(*numeric_phredscores)
 
-def calculate_average_phredscores(all_phredscores: dict):
-        """
-        Calculate the average phredscore per column.
-        param: all_phredscores, a list with defaultdictionaries with for each base position a phredscore.
-        The number of chunks equals to the number of defaultdictionaries.
-        :return: A dictionary with for each base position (key) the average phredscore as value.
-        """
-        return [
-            {
-                "line_number": base_position,
-                "average_phredscore": sum(phredscores) / len(phredscores),
-            }
-            for base_position, phredscores in sorted(all_phredscores.items())
-        ]
+def calculate_averages():
+    counts_per_basepair = []
+    total_sums_per_basepair = []
+    line_count = 0
 
-def write_results(filename: str, average_scores: list, outputfile: str):
-        fieldnames = ["line_number", "average_phredscore"]
-        if outputfile:
-            with open(outputfile, mode="a", encoding="UTF-8") as csvfile:
-                writer = csv.DictWriter(
-                    csvfile, fieldnames=fieldnames
-                )  # use csv object for writing to a csv file
-                csvfile.write(filename + "\n")
-                for phredscores in average_scores:
-                    writer.writerow(phredscores)
-                csvfile.write("\n")
-            print(
-                f"--Successfully written {filename} mean phredscores to {outputfile}--"
-            )
-        else:  # print instead of write
-            print(filename + "\n")
-            for phredscores in average_scores:
-                print(
-                    f"{phredscores['line_number']},{phredscores['average_phredscore']}"
-                )
-            print("\n")
-
-def process_chunk():
-    phred_scores, headers = convert_binary_to_phredscores(sys.stdin.buffer.read())
-    writer = csv.DictWriter(sys.stdout, fieldnames=range(max(headers)))
-    writer.writeheader()
-    for base_position_score in range(len(phred_scores)):
-       writer.writerow(phred_scores[base_position_score])
-
+    for line in sys.stdin:
+        if line.strip():
+            scores = [int(score) for score in line.split()]
+            if not any(counts_per_basepair):
+                counts_per_basepair = np.zeros(len(scores), dtype=int)
+                total_sums_per_basepair = np.zeros(len(scores), dtype=int)
+            counts_per_basepair += 1
+            total_sums_per_basepair += scores
+            line_count += 1
+    means = total_sums_per_basepair / counts_per_basepair
+    for i, mean in enumerate(means, start=1):
+        print(f"{i},{mean}")
 
 if __name__ == "__main__":
-    process_chunk()
+    args = process_prompt_params()
+    if args.calc:
+        convert_binary_to_phredscores()
+        #process_chunk()
+    if args.mean:
+        calculate_averages()
