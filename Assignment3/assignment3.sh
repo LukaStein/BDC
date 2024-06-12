bb4ecff31d51ad6d5ef4194e625e5a7f14f70508
L Stein <60214213+LukaStein@users.noreply.github.com>
2024-06-12 19:12:46 +0200

    Bash script now parallelizes several fastq files
    TODO: add save to output file functionality

--- Assignment3/assignment3.sh
+++ Assignment3/assignment3.sh
@@ -3,8 +3,20 @@
 # Locate the location of this bash script, and assumely the python script
 LOCATE_DIR=$(dirname -- "$( readlink -f -- "$0"; )")
 
-FILE_PATH=$1
-CORES=$2
+CORES=$1
+# Moves the next argument, which should be a fastq file to $1 of the parameter array.
+shift
 
+process_fastq_to_meanscores () {
+  local fastq_file=$1
 
-awk 'NR % 4 == 0' $FILE_PATH | parallel --pipe --sshloginfile parallel_ssh --block 5M --memfree 1G -j $CORES python3 ${LOCATE_DIR}/assignment3.py -calc {} | python3 ${LOCATE_DIR}/assignment3.py -mean {}
+  awk 'NR % 4 == 0' "$fastq_file" | parallel --pipe --sshloginfile parallel_ssh --block 5M --memfree 1G -j "$CORES" python3 ${LOCATE_DIR}/assignment3.py -calc {} | python3 ${LOCATE_DIR}/assignment3.py -mean {}
+}
+
+# Make function accessible to be used in parallelization and variables accessible from within the function
+export -f process_fastq_to_meanscores
+export LOCATE_DIR
+export CORES
+
+# Invoke function parallel for all files in the arguments
+parallel process_fastq_to_meanscores ::: "$@"
