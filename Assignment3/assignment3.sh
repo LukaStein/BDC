8b6383e4f3292e8e9f7436eebcab8b7d2559e009
L Stein <60214213+LukaStein@users.noreply.github.com>
2024-06-13 11:49:56 +0200

    Changed approach to write output to CSV files

--- Assignment3/assignment3.sh
+++ Assignment3/assignment3.sh
@@ -10,7 +10,7 @@ shift
 process_fastq_to_meanscores () {
   local fastq_file=$1
   local full_filename=$(basename "$fastq_file")
-  awk 'NR % 4 == 0' "$fastq_file" | parallel --pipe --sshloginfile parallel_ssh --block 5M --memfree 1G -j "$CORES" python3 ${LOCATE_DIR}/assignment3.py -calc -filename "$full_filename" {} | python3 ${LOCATE_DIR}/assignment3.py -mean -filename "$full_filename" {}
+  awk 'NR % 4 == 0' "$fastq_file" | parallel --pipe --sshloginfile parallel_ssh --block 5M --memfree 1G -j "$CORES" python3 ${LOCATE_DIR}/assignment3.py -calc -filename "$full_filename" {} | python3 ${LOCATE_DIR}/assignment3.py -mean -o "$#" -filename "$full_filename" {}
 }
 
 # Make function accessible to be used in parallelization and variables accessible from within the function
@@ -19,4 +19,4 @@ export LOCATE_DIR
 export CORES
 
 # Invoke function parallel for all files in the arguments
-parallel process_fastq_to_meanscores ::: "$@" 1> output.csv
+parallel process_fastq_to_meanscores ::: "$@"
