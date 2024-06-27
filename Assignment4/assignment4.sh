b18848f92324f277bc1bfd04a9f5b8e1c132ffd0
L Stein <60214213+LukaStein@users.noreply.github.com>
2024-06-27 09:17:00 +0200

    Changed encoding to unix encoding

--- Assignment4/assignment4.sh
+++ Assignment4/assignment4.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --job-name=slorm_a4
+#SBATCH --job-name=BOOO_a4
 # run for max five minutes
 # #            d-hh:mm:ss
 #SBATCH --time 0-00:10:00
@@ -15,4 +15,7 @@
 source /commons/conda/conda_load.sh
 
 THISDIR=$(dirname -- "$( readlink -f -- "$0"; )")
-mpiexec -n $SLURM_NTASKS python3 assignment4.py -c 10 /commons/Themas/Thema12/HPC/rnaseq.fastq
+mpiexec -n $SLURM_NTASKS python3 assignment4.py -c 10
+#/commons/Themas/Thema12/HPC/rnaseq.fastq
+
+
